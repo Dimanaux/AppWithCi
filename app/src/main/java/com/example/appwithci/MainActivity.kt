1f28d12c1abd19461e0d009a0a49b10b78e85966
cosmoskey <barskoidim@gmail.com>
2019-02-23 13:05:14 +0300

    initial commit

---
+++ app/src/main/java/com/example/appwithci/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.example.appwithci
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    val value =
+        "Hello world!" + "\n this string is tooooooo loooooooooooooooooooooooooooooooooooooooong, isn't it???????????????????????????????????"
+}
+
+
+
+
+
+
+
